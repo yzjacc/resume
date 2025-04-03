1b68886054794be09279911a7ebabe2eb0deb1bd
yzjacc <yzjacc@gmail.com>
2025-04-03 10:41:52 +0800

    feat: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import { viteSingleFile } from 'vite-plugin-singlefile';
-// 若要在当前仓库设置用户名，可以使用以下命令：
-// git config --local user.name "Your Name"
-// 若要在当前仓库设置邮箱，可以使用以下命令：
-// git config --local user.email "your.email@example.com"
-
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './src',
